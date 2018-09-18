a07228cc7cccb803531d4cb0f0cfad6518ac9b68
Alejandro Fischer <alefisch@Alejandros-MacBook-Pro.local>
2018-09-18 16:53:01 -0400

    fail and pass files added

---
+++ failTechComm.py
@@ -0,0 +1,13 @@
+def howToFailThisClass():
+    x = 15
+    y = 0
+    while x > y:
+        print("Do not do any work!")
+        x = x - 1
+        y = y + 1
+
+def main():
+    howToFailThisClass()
+
+if __name__ == "__main__":
+    main()
---
+++ passTechComm.py
@@ -0,0 +1,13 @@
+def howToPassThisClass():
+    x = 15
+    y = 0
+    while x > y:
+        print("Work hard!")
+        x = x - 1
+        y = y + 1
+
+def main():
+    howToPassThisClass()
+
+if __name__ == "__main__":
+    main()
